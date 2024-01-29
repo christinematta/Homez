4e43ad0e38c24ffcd62f791ec673de3938881000
Christine Matta <christine.onsi@gmail.com>
2024-01-29 14:40:06 +0100

    implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-
+import React from 'react';
+import NavBar from './components/NavBar/NavBar'
+import Hero from './components/Hero/Hero'
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+  <div className="App">
+    <NavBar/>
+    <Hero/>
+  </div>
   );
 }
 
