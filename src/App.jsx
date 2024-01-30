10b294870ca5307d2f32a69da600b43bbe66c94c
Christine Matta <christine.onsi@gmail.com>
2024-01-30 13:10:28 +0100

    Finished hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import NavBar from './components/NavBar/NavBar'
-import Hero from './components/Hero/Hero'
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import './utils/App.css'
 function App() {
   return (
-  <div className="App">
-    <NavBar/>
-    <Hero/>
-  </div>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <NavBar />
+        <Hero />
+      </div>
+    </div>
   );
 }
 
