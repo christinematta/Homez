9320b03df8af7fb060ccbfb5834f90bb54c7f3cd
Christine Matta <christine.onsi@gmail.com>
2024-01-30 13:43:00 +0100

    Implement companies component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import './utils/App.css'
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
+        <div className="green-gradient"/>
         <NavBar />
         <Hero />
+
       </div>
+      <Companies/>
     </div>
   );
 }
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,13 @@
+import './Companies.css'
+export default function Companies() {
+  return (
+    <section className="c-wrapper">
+      <div className="innerWidth padding flexCenter c-container">
+        <img src="./equinix.png" alt="" />
+        <img src="./realty.png" alt="" />
+        <img src="./prologis.png" alt="" />
+        <img src="./tower.png" alt="" />
+      </div>
+    </section>
+  );
+}
