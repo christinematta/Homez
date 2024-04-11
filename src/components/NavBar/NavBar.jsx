57c8c4370f09ba637e54f98ba43cd40f6cb84771
Christine Matta <christine.onsi@gmail.com>
2024-04-11 11:13:49 +0200

    Some view inhancement

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,10 +5,17 @@ import OutsideClickHandler from "react-outside-click-handler";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [display, setDisplay] = useState();
+
+  function handleMenudisplay() {
+
+  }
+
   function getMenuStyles() {
     if (document.documentElement.clientWidth <= 875) {
       return {
         right: !menuOpen && "-100%",
+        // visibility:menuOpen && "visible"
       };
     }
   }
@@ -16,20 +23,23 @@ export default function Header() {
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./Homez.png" alt="logo" width={200} />
-        <OutsideClickHandler  onOutsideClick={() => setMenuOpen(false)}>
-        <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get Started</a>
-          <button>
-            <a href="">Contact</a>
-          </button>
-        </div>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button>
+              <a href="">Contact</a>
+            </button>
+          </div>
 
-        <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
-          <BiMenuAltRight size={30} color={"rgba(75, 207, 141, 1)"} />
-        </div>
+          <div
+            className="menu-icon"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            <BiMenuAltRight size={30} color={"rgba(75, 207, 141, 1)"} />
+          </div>
         </OutsideClickHandler>
       </div>
     </section>
